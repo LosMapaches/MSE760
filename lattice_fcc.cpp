e5e074ad3d19fd25346903d44fb9c4d2b28f0399
mse760f18 <mse760f18@korczak2.msae.wisc.edu>
2018-09-09 08:57:56 -0500

    First commit

---
+++ lattice_fcc.cpp
@@ -0,0 +1,20 @@
+/*This script was written by Lane Schultz for MSE 760 */
+/*Cite me if you got help here*/
+
+int lattice_fcc(int n, int a)
+{
+    // Return the x, y, and z coordinates of atoms in FCC
+
+
+    int d = 3;                    // Dimensions of the problem
+
+    int x [n] = {};               // x coordinates of atoms
+    int y [n] = {};               // y coordinates of atoms
+    int z [n] = {};               // z coordinatse of atoms
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << x;
+    }
+    return 0;
+}
