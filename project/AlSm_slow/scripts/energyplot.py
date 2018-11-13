6793d5b22139a84d5dbe51f86f998455618fcd90
Lane <love.allsciences@gmail.com>
2018-11-13 12:11:55 -0600

    improved scripts

--- project/AlSm_slow/scripts/energyplot.py
+++ project/AlSm_slow/scripts/energyplot.py
@@ -2,6 +2,7 @@ from scipy.interpolate import UnivariateSpline
 from matplotlib import pyplot as pl
 import pandas as pd
 import numpy as np
+import math
 
 from dataimport import importdata as data
 
@@ -27,22 +28,53 @@ for run in runs:
 
     fig, sub = pl.subplots(1, 1)
     xdata = list(runs[run]['c_mytemp'])
-    sub.plot(xdata, term, '.', label='Data Points')
+    sub.plot(xdata, term, 'b.', label='Data Points')
+
+    # Find bottom percent of data to fit
+    length = len(xdata)
+    start0 = 0
+    end0 = math.floor(0.1*length)
+
+    x0 = xdata[start0:end0]
+    y0 = term[start0:end0]
+    fit0 = np.polyfit(x0, y0, 1)
+    fitf0 = np.poly1d(fit0)
+    yfit0 = fitf0(xdata)
+
+    fitrange0 = str([math.floor(xdata[start0]), math.floor(xdata[end0])])
+    sub.plot(xdata, yfit0, 'r', label='Fit Range of '+fitrange0+' [K]')
+
+    # Find top percent of data to fit
+    start1 = math.floor(0.3*length)
+    end1 = -1
+    x1 = xdata[start1:end1]
+    y1 = term[start1:end1]
+    fit1 = np.polyfit(x1, y1, 1)
+    fitf1 = np.poly1d(fit1)
+    yfit1 = fitf1(xdata)
+
+    fitrange1 = str([math.floor(xdata[start1]), math.floor(xdata[end1])])
+    sub.plot(xdata, yfit1, 'c', label='Fit Range of '+fitrange1+' [K]')
 
     try:
-        npoints = 100
-        kterm = 4
-        xfit = np.linspace(min(xdata), max(xdata), npoints)
-        spl = UnivariateSpline(xdata, term, k=kterm)
-        spl.set_smoothing_factor(1e4)
-        sub.plot(xfit, spl(xfit), label='Spline Fit (k='+str(kterm)+')')
+        npoints = 2
+        point = [0]*(npoints+1)  # A point larger than 1 to start with
+        tolerance = 0.01  # The initial tolerance
+
+        # Iterate until point of intersection is found between fits
+        while len(point) > npoints:
+            point = np.argwhere(np.isclose(yfit0, yfit1, atol=tolerance)).reshape(-1)
+            tolerance -= 0.0005
+
+        temps = np.array(xdata)[point]
+        temp = math.floor(sum(temps)/len(temps))
+        sub.axvline(x=temp, linestyle='--', label='Fit Intersection at '+str(temp)+' [K]')
 
     except Exception:
         pass
 
     sub.set_xlabel('Temperature [K] for '+percentage)
     sub.set_ylabel('E-3*k_b*T [eV/atom]')
-    sub.legend(loc='best')
+    sub.legend(loc='upper left')
     sub.grid()
-
-pl.show()
+    fig.savefig('../figures/AlSm_slow_'+run)
